23c711c722b49ed6a6ea096be0557d1e2b515e73
Joan F <joanfoubertetude@gmail.com>
2020-01-30 09:50:22 +0100

    robotDone

---
+++ sanAntonio/characters_spider.py
@@ -0,0 +1,9 @@
+import scrapy
+
+class BlogSpider(scrapy.Spider):
+    name = 'blogspider'
+    start_urls = ['https://fr.wikipedia.org/wiki/Cat%C3%A9gorie:Personnage_d%27animation']
+
+    def parse(self, response):
+        for title in response.css('div#mw-pages div.mw-content-ltr a'):
+            yield {'character': title.css('a ::text').get()}
